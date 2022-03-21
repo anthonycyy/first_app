6e4b9f6e9b0b30b6184264435f4292b6d6335f79
anthonycyy <74084720+anthonycyy@users.noreply.github.com>
2022-03-21 14:57:56 +0000

    Update app2.py

--- app2.py
+++ app2.py
@@ -4,3 +4,10 @@ st.title("Hello")
 st.header("DS4 is the best")
 my_str=st.text_input("name?")
 st.text(f'Hello {my_str}')
+
+def f(my_str):
+  if len(my_str)>10:
+    return "long string"
+  else:
+    return "short string"
+st.text(f'{f(my_str)}')
