23ed736208d50752cdbacfd5f7a6786937aea00a
anthonycyy <74084720+anthonycyy@users.noreply.github.com>
2022-03-21 15:01:03 +0000

    Update app2.py

--- app2.py
+++ app2.py
@@ -3,11 +3,11 @@ import streamlit as st
 st.title("Hello")
 st.header("DS4 is the best")
 my_str=st.text_input("name?")
-st.text(f'Hello {my_str}')
+st.text(f'My string is {my_str}')
 
 def f(my_str):
   if len(my_str)>10:
     return "long string"
   else:
     return "short string"
-st.text(f'{f(my_str)}')
+st.text(f(my_str))
