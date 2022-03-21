4e9a8a80dbec1eaa3e39bf9960272798c058f437
anthonycyy <74084720+anthonycyy@users.noreply.github.com>
2022-03-21 17:05:30 +0000

    Update app_vs.py

--- app_vs.py
+++ app_vs.py
@@ -6,6 +6,16 @@ st.header("Data science is the best")
 my_str=st.text_input("name?")
 st.text(f'My string is {my_str}')
 
+click_ans=st.button("enter")
+def f(my_str,threshold=10):
+    st.text(f'My string is {my_str}')
+    if len(my_str)>threshold:
+        st.text( "long string")
+    else:
+        st.text( "short string")
+if click_ans:
+    f(my_str)
+
 click=st.button("snow")
 arr=[i for i in range(10)]
 bar=st.sidebar.select_slider('slide it',options=arr)
@@ -13,12 +23,3 @@ bar=st.sidebar.select_slider('slide it',options=arr)
 if click:
   st.snow()
   st.text('snow is coming')
-
-click_ans=st.button("enter")
-def f(my_str,threshold=10):
-  if len(my_str)>threshold:
-    return ( "long string")
-  else:
-    return ( "short string")
-if click_ans:
-    st.text(f(my_str))
