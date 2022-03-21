8b25d24f1b17ab36a84b43e9c9fc9ea86c07de22
anthonycyy <74084720+anthonycyy@users.noreply.github.com>
2022-03-21 16:51:23 +0000

    Create app_vs.py

---
+++ app_vs.py
@@ -0,0 +1,20 @@
+import streamlit as st
+import pandas as pd
+
+st.title("Hello")
+st.header("Data science is the best")
+my_str=st.text_input("name?")
+st.text(f'My string is {my_str}')
+
+click=st.button("press me")
+bar=st.sidebar("Swipe me")
+
+if click:
+  st.snow()
+
+def f(my_str,threshold=10):
+  if len(my_str)>threshold:
+    return ( "long string")
+  else:
+    return ( "short string")
+st.text(f(my_str))
