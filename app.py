71d2fc4688635716dc2142ab6e5101bf4506c6fe
anthonycyy <74084720+anthonycyy@users.noreply.github.com>
2022-03-22 11:05:16 +0000

    Update app.py

--- app.py
+++ app.py
@@ -9,20 +9,26 @@ for i in range(6):
     input_text="Input a number (from 1 to {}):".format(49)
     number=st.number_input(input_text,step=1,key=i)
     my_nums.append(number)
+input_stext="Input a special number (from 1 to {}):".format(49)
+my_special_number=st.number_input(input_stext,step=1)
 click=st.button("Start drawing: ")
 all_nums=[i+1 for i in range(49)]
-def draw_mark_6(my_nums):
-    my_nums.sort()
-    nums=list(np.random.choice(all_nums,size=6,replace=False))
-    nums.sort()
+def draw_mark_6(my_nums,my_special_number):
+    nums=list(np.random.choice(all_nums,size=7,replace=False))
+    nums,special_num=nums[:-1],nums[-1]
     st.text("numbers drawn are:")
-    st.text(nums)
+    st.text(nums,special_num)
     st.text("your numbers are: ")
-    st.text(my_nums)
+    st.text(my_nums,my_special_number)
+    nums=set(nums)
+    my_nums=set(my_nums)
     if nums==my_nums:
-        return "success"
-    else:
-        return "fail"
+        return "first_prize"
+    elif len(nums.intersection(my_nums))==5:
+        if my_special_number==special_num:
+            return "second prize"
+        return "third prize"
+    return "nothing"
 if click:
-    result=draw_mark_6(my_nums)
+    result=draw_mark_6(my_nums,my_special_number)
     st.text(result)
