1b4c29b8ebfae89dbf664459b09e5d2fe682f458
anthonycyy <74084720+anthonycyy@users.noreply.github.com>
2022-03-22 11:14:50 +0000

    Update app.py

--- app.py
+++ app.py
@@ -5,17 +5,18 @@ import pandas as pd
 st.title("Welcome to mark six")
 st.header("Pick 6 numbers")
 my_nums=[]
-for i in range(6):
+size=st.number_input("Input size:",step=1)
+for i in range(size):
     input_text="Input a number (from 1 to {}):".format(49)
     number=st.number_input(input_text,step=1,key=i)
     my_nums.append(number)
 st.header("Pick a special number")
 input_stext="Input a special number (from 1 to {}):".format(49)
-my_special_number=st.number_input(input_stext,step=1,key=6)
+my_special_number=st.number_input(input_stext,step=1)
 click=st.button("Start drawing: ")
 all_nums=[i+1 for i in range(49)]
-def draw_mark_6(my_nums,my_special_number):
-    nums=list(np.random.choice(all_nums,size=7,replace=False))
+def draw_mark_6(my_nums,my_special_number,size):
+    nums=list(np.random.choice(all_nums,size=size+1,replace=False))
     nums,special_num=nums[:-1],nums[-1]
     st.text("numbers drawn are:")
     st.text([nums,special_num])
@@ -25,11 +26,11 @@ def draw_mark_6(my_nums,my_special_number):
     my_nums=set(my_nums)
     if nums==my_nums:
         return "first_prize"
-    elif len(nums.intersection(my_nums))==5:
+    elif len(nums.intersection(my_nums))==size-1:
         if my_special_number==special_num:
             return "second prize"
         return "third prize"
     return "nothing"
 if click:
-    result=draw_mark_6(my_nums,my_special_number)
+    result=draw_mark_6(my_nums,my_special_number,size)
     st.text(result)
