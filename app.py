06684204a71dbfe071d2ecaa892150afd94d7c07
anthonycyy <74084720+anthonycyy@users.noreply.github.com>
2022-03-22 09:40:02 +0000

    Update app.py

--- app.py
+++ app.py
@@ -7,5 +7,11 @@ st.header("Pick 6 numbers")
 my_nums=[]
 for i in range(6):
     number=st.number_input("Input a number: ",step=1,key=i)
-    # my_nums.append(number)
-# click_ans=st.button("enter")
+    my_nums.append(number)
+click=st.button("Start drawing: ")
+all_nums=[i+1 for i in range(49)]
+def draw_mark_6():
+    all_nums=np.random.choice(all_nums,size=6,replace=False)
+    print(all_nums)
+if click:
+    draw_mark_6()
